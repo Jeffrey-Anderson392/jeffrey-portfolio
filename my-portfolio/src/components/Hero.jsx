b317c11c923b27e2e1bc8f1ad315e84c3d52c0f9
Jeffrey Anderson <159304590+Jeffrey-Anderson392@users.noreply.github.com>
2025-11-20 17:41:34 -0600

    added main file structure layout and had to rename components due to improper naming technique

---
+++ my-portfolio/src/components/Hero.jsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+  return (
+    <section className="flex flex-col items-center text-center mt-20 px-6">
+      <h1 className="text-5xl font-bold text-blue-600">Hi, I'm Jeffrey</h1>
+      <p className="mt-4 text-gray-700">Welcome to my portfolio</p>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ my-portfolio/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Hero from '../components/Hero';
+import Contact from '../components/Contact';
+
+export default function Home() { 
+    return (
+        <>
+        <Hero />
+        </>
+    );
+}
