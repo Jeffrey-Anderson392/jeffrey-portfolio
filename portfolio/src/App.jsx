5056146b1f843682643b4e61448b0f29daca8e20
Jeffrey Anderson <159304590+Jeffrey-Anderson392@users.noreply.github.com>
2025-11-20 19:53:31 -0600

    added Hero component and imported into App.jsx. initial commit because things are working right now

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Hero from './components/Hero.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+    <Hero />
+    {/* commenting this out for now because it is annoying me but i winder if deleting it will break everything */}
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -30,7 +33,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
---
+++ portfolio/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+        <div className="bg-neutral-800 text-2xl text-blue-100">Jeffrey Anderson</div>
+    </div>
+  )
+}
+
+export default Hero
