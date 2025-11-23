5583171b42cb7376e95c78112add3c5f3d79aad8
Jeffrey Anderson <159304590+Jeffrey-Anderson392@users.noreply.github.com>
2025-11-22 21:55:06 -0600

    wrote in my about me section and imported new font to use across the webpage

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -16,13 +16,13 @@ const Navbar = () => {
 
   return (
     <div>
-        <nav className="p-6 flex justify-between text-blue-100 font-mono">
+        <nav className="p-6 flex justify-between text-blue-100 font-plus-jakarta-sans">
             <div className="text-3xl font-semibold">Jeffrey Anderson</div>
             <div className="space-x-10 text-2xl p-0.5">
-                <a href="#home" className="hover:text-slate-500 " onClick={(e) => handleclick(e, 'home')}>Home</a>
-                <a href="#about" className="hover:text-slate-500" onClick={(e) => handleclick(e, 'about')}>About</a>
-                <a href="#projects" className="hover:text-slate-500" onClick={(e) => handleclick(e, 'projects')}>Projects</a>
-                <a href="#contact" className="hover:text-slate-500" onClick={(e) => handleclick(e, 'contact')}>Contact</a>
+                <a href="#home" className="hover:text-teal-300 " onClick={(e) => handleclick(e, 'home')}>Home</a>
+                <a href="#about" className="hover:text-teal-300" onClick={(e) => handleclick(e, 'about')}>About</a>
+                <a href="#projects" className="hover:text-teal-300" onClick={(e) => handleclick(e, 'projects')}>Projects</a>
+                <a href="#contact" className="hover:text-teal-300" onClick={(e) => handleclick(e, 'contact')}>Contact</a>
             </div>
         </nav>
     </div>
